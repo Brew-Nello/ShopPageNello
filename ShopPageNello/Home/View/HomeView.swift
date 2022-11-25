780b2d86ba1aa710d2536791599a4a57a6a0f6ab
Brew-Nello <trave9360@gmail.com>
2022-11-25 10:18:09 +0900

    feat: 홈 화면 구현 진행중

--- ShopPageNello/Home/View/HomeView.swift
+++ ShopPageNello/Home/View/HomeView.swift
@@ -8,13 +8,57 @@
 import UIKit
 
 /// 홈 화면
-class HomeView: UIView {
+final class HomeView: UIView {
+    
     // MARK: - UI
     
-    let mainCollectionView = UICollectionView()
-    let bannerCollectionView = UICollectionView()
+    let collectionView: UICollectionView = {
+        let collectionViewFlowLayout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 0
+        collectionViewFlowLayout.scrollDirection = .vertical
+        collectionViewFlowLayout.minimumInteritemSpacing = spacing
+        collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.backgroundColor = .blue
+        return collectionView
+    }()
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        registerCell()
+        addSubviews()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    private func configure() {
+        backgroundColor = .white
+    }
     
     
+    // MARK: - Actions
+    func registerCell() {
+        collectionView.register(HomeBannerViewCell.self, forCellWithReuseIdentifier: HomeBannerViewCell.identifier)
+        collectionView.register(HomeItemViewCell.self, forCellWithReuseIdentifier: HomeItemViewCell.identifier)
+    }
     
+    // MARK: - Layouts
+    func addSubviews() {
+        addSubview(collectionView)
+    }
     
+    func setLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
 }
+
